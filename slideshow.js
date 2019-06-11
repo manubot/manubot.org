8a07fddd80727025a22a7f5578bd22e9a9232b4e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-06-11 18:12:52 -0400

    add automated images

--- slideshow.js
+++ slideshow.js
@@ -1,6 +1,12 @@
 const slideshows = document.querySelectorAll('.slideshow');
-for (const slideshow of slideshows)
+for (const slideshow of slideshows) {
     cycleSlideshow(slideshow);
+    slideshow.addEventListener('click', () => {
+        window.clearInterval(slideshow.timer);
+        cycleSlideshow(slideshow);
+    });
+    slideshow.timer = window.setInterval(() => cycleSlideshow(slideshow), 3000);
+}
 
 function cycleSlideshow(slideshow) {
     const first = slideshow.firstElementChild;
@@ -12,9 +18,4 @@ function cycleSlideshow(slideshow) {
 
     if (next || first)
         (next || first).setAttribute('data-active', '');
-
-    window.setTimeout(
-        () => cycleSlideshow(slideshow),
-        slideshow.dataset.time || 3000
-    );
 }
