5dce1aa481bdf46be6c4dc83ba04c1a71c37b1ce
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-09-17 16:00:29 -0400

    add title to carousel images (#32)
    - adds paper title and link to manubot html version of paper to catalog carousel images on hover
    - tweaks the carousel behavior code to accommodate the above
    - removes globe icon from catalog page thumbnails
    - tweaks and cleans some css

--- slideshow.js
+++ slideshow.js
@@ -1,37 +1,45 @@
 const slideshows = document.querySelectorAll('.slideshow');
 for (const slideshow of slideshows) {
+  cycleSlideshow(slideshow);
+  slideshow.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'IMG') return;
+    window.clearInterval(slideshow.timer);
     cycleSlideshow(slideshow);
-    slideshow.addEventListener('click', () => {
-        window.clearInterval(slideshow.timer);
-        cycleSlideshow(slideshow);
-    });
-    slideshow.timer = window.setInterval(() => cycleSlideshow(slideshow), 3000);
+  });
+  slideshow.timer = window.setInterval(() => cycleSlideshow(slideshow), 3000);
 }
 
 function cycleSlideshow(slideshow) {
-    const first = slideshow.firstElementChild;
-    const current = slideshow.querySelector('[data-active]');
-    const next = current ? current.nextElementSibling : undefined;
+  const first = slideshow.firstElementChild;
+  const current = slideshow.querySelector('[data-active]');
+  const next = current ? current.nextElementSibling : undefined;
 
-    for (const child of slideshow.children)
-        hideSlide(child);
+  for (const child of slideshow.children) hideSlide(child);
 
-    showSlide(next || first);
+  showSlide(next || first);
 }
 
 function showSlide(element) {
-    if (!element)
-        return;
+  if (!element) return;
 
-    element.setAttribute('data-active', '');
-    if (!element.src && element.dataset.src) {
-        element.src = element.dataset.src;
+  element.setAttribute('data-active', '');
+  if (!element.src && element.dataset.src) {
+    element.src = element.dataset.src;
+  }
+  const children = element.children;
+  for (const child of children) {
+    if (!child.src && child.dataset.src) {
+      child.src = child.dataset.src;
     }
+  }
 }
 
 function hideSlide(element) {
-    if (!element)
-        return;
-
-    element.removeAttribute('data-active');
+  if (!element) return;
+  element.removeAttribute('data-active');
+  if (!element.children.length) return;
+  const children = element.children;
+  for (const child of children) {
+    child.removeAttribute('data-active');
+  }
 }
